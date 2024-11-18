dacb412f008a1cf69f12ad0ca810ac65b1546b61
vedpatel <uoewv2@gmail.com>
2024-11-18 08:23:50 +0530

    test

--- my-app/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://lvytjfabodewspdyxxas.supabase.co';
-const supabaseAnonKey = process.env.SUPABASE_KEY!;  // Non-null assertion
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
